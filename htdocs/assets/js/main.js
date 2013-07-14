36d8a89e56c6bff10c14d6507c9a27f551df28bd
moyashipan <moyashipan@gmail.com>
2013-07-14 12:37:53 +0900

    盤上をクリックしたらランダムにピース置けるように

--- htdocs/assets/js/main.js
+++ htdocs/assets/js/main.js
@@ -9,75 +9,85 @@ Player.prototype = {
 	drawDeck: function() {
 		if (!this.deck) return;
 		var piece = $('.deck-piece-template').clone().removeClass('deck-piece-template').addClass('deck-piece');
-		var container = $('.deck' + this.id);
+		var container = $('.deck' + this.id).find('.deck-pieces');
 		for (var i in this.deck) {
 			container.append(piece.clone().addClass(this.deck[i].name));
 		}
 	}
 };
 
+// TODO:後でクラスにする...
+var player_turn = 0;
+var max_player = 4;
+
+var deck_piece_templates = {
+	straight:{
+		type    :'path',
+		body    :[[0,0]],
+		inputs  :[[0,1]],
+		outputs :[[0,-1]]
+	},
+	left_curve:{
+		type    :'path',
+		body    :[[0,0]],
+		inputs  :[[0,1]],
+		outputs :[[-1,0]]
+	},
+	right_curve:{
+		type    :'path',
+		body    :[[0,0]],
+		inputs  :[[0,1]],
+		outputs :[[1,0]]
+	},
+	left_turn:{
+		type    :'path',
+		body    :[[0,0],[1,0]],
+		inputs  :[[1,1]],
+		outputs :[[0,1]]
+	},
+	right_turn:{
+		type    :'path',
+		body    :[[0,0],[1,0]],
+		inputs  :[[0,1]],
+		outputs :[[1,1]]
+	},
+	side_branch:{
+		type    :'path',
+		body    :[[0,0]],
+		inputs  :[[0,1]],
+		outputs :[[1,0],[-1,0]]
+	},
+	triple_branch:{
+		type    :'path',
+		body    :[[0,0]],
+		inputs  :[[0,1]],
+		outputs :[[1,0],[-1,0],[0,-1]]
+	},
+	block:{
+		type    :'block',
+		body    :[[0,0]],
+		inputs  :[],
+		outputs :[]
+	}
+};
+var piece_names = [];
+for (var key in deck_piece_templates) {
+	if (key == undefined) continue;
+	piece_names.push(key);
+}
+
+var deck_piece_names = [
+	'straight', 'straight',
+	'left_curve','left_curve','left_curve',
+	'right_curve','right_curve','right_curve',
+	'left_turn',
+	'right_turn',
+	'side_branch',
+	'triple_branch',
+	'block'
+];
+
 (function($){
-	var deck_piece_templates = {
-		straight:{
-			type    :'path',
-			body    :[[0,0]],
-			inputs  :[[0,1]],
-			outputs :[[0,-1]]
-		},
-		right_curve:{
-			type    :'path',
-			body    :[[0,0]],
-			inputs  :[[0,1]],
-			outputs :[[1,0]]
-		},
-		left_curve:{
-			type    :'path',
-			body    :[[0,0]],
-			inputs  :[[0,1]],
-			outputs :[[-1,0]]
-		},
-		right_turn:{
-			type    :'path',
-			body    :[[0,0],[1,0]],
-			inputs  :[[0,1]],
-			outputs :[[1,1]]
-		},
-		left_turn:{
-			type    :'path',
-			body    :[[0,0],[1,0]],
-			inputs  :[[1,1]],
-			outputs :[[0,1]]
-		},
-		side_branch:{
-			type    :'path',
-			body    :[[0,0]],
-			inputs  :[[0,1]],
-			outputs :[[1,0],[-1,0]]
-		},
-		triple_branch:{
-			type    :'path',
-			body    :[[0,0]],
-			inputs  :[[0,1]],
-			outputs :[[1,0],[-1,0],[0,-1]]
-		},
-		block:{
-			type    :'block',
-			body    :[[0,0]],
-			inputs  :[],
-			outputs :[]
-		}
-	};
-	var deck_piece_names = [
-		'straight', 'straight',
-		'right_curve','right_curve','right_curve',
-		'left_curve','left_curve','left_curve',
-		'right_turn',
-		'left_turn',
-		'side_branch',
-		'triple_branch',
-		'block'
-	];
-	
 	// 各プレイヤーに配るデッキを作成	
 	var deck = [];
 	for (var i in deck_piece_names) {
@@ -112,7 +122,16 @@ Player.prototype = {
 		for (var y=0; y<10; y++) {
 			for (var x=0; x<10; x++) {
 				base_pieces.append(piece.clone().removeClass('piece-template').addClass('piece'));
-				pieces.append(piece.clone().removeClass('piece-template').addClass('piece'));
+				var _piece = piece.clone().removeClass('piece-template').addClass('piece');
+				_piece.on('click', function(e){
+					var type = piece_names[Math.floor(Math.random() * piece_names.length)];
+					$(this).removeClass(function(index, css) {
+						return (css.match(/\b(type\_|player)\S+/g) || []).join(' ');
+					});
+					$(this).addClass('type_' + type).addClass('player' + player_turn);
+					player_turn = (player_turn + 1) % max_player;
+				}); 
+				pieces.append(_piece);
 			}
 		}
 
